c6963e8442c66c9bd53aa23251ab86de2a479b60
Jesus <a00835462@tec.mx>
2024-03-20 10:37:43 -0600

    Primer commit de la app de habits

---
+++ HabitsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitsApp
+//
+//  Created by Alumno on 20/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Bienvenidos a Habits!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitsApp/HabitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsAppApp.swift
+//  HabitsApp
+//
+//  Created by Alumno on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

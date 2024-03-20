32746c63984d9504bc7626e831811d28b8317a9a
Jesus <a00835462@tec.mx>
2024-03-20 11:43:54 -0600

    Cambie texto

--- HabitsApp/ContentView.swift
+++ HabitsApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Bienvenidos a Habits!")
+            Text("Bienvenidos Habits!")
         }
         .padding()
     }

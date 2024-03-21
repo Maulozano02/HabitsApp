eb7fd13b6e96b986b13e8fb2168870f87c86d16f
Mauricio Lozano <maulozano02@gmail.com>
2024-03-21 13:20:24 -0600

    cambios

--- HabitsApp_Final/HomePageView.swift
+++ HabitsApp_Final/HomePageView.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 struct HomePageView: View {
     let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-        let dayToHighlight = "Thu"
+    let dayCompleted = ["Sun", "Tue"]
+    let dayToHighlight = "Thu"
+    @State private var selectedDays: Set<Date> = []
+    let daysInMonth = 30
+    
     var body: some View {
         VStack {
             HStack {
@@ -26,49 +30,100 @@ struct HomePageView: View {
                     .padding(.horizontal, 10)
             }
             .padding(.horizontal)
-            .padding(.top, 50)
+                    .padding(.top, 50)
             
-            // HStack con fondo morado
+
+            // Tu HStack con el fondo morado
             HStack {
                 VStack {
-                    Text("Need improvement in Wednesday")
-                    Text("Need improvement in Wednesday")
-                    Text("Need improvement in Wednesday")
+                    Text("Need more sleep                            ")
+                    Spacer()
+                    Text("Stay hydrated                                 ")
+                    Spacer()
+                    Text("Study is the most strong habit     ")
                 }
                 .foregroundColor(.white)
-                .padding(10)
+                .padding(.vertical, 40)
+                .padding(.horizontal, 10)
                 
                 Spacer()
-                Image("Logo")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .padding(10)
+                VStack {
+                    Text("50%")
+                    Spacer()
+                    Text("20%")
+                    Spacer()
+                    Text("100%")
+                }
+                .foregroundColor(.white)
+                .padding(.vertical, 40)
+                .padding(.horizontal, 10)
             }
-            .frame(maxWidth: .infinity) // Extiende al máximo ancho.
+            .frame(maxWidth: .infinity)
             .background(
                 LinearGradient(gradient: Gradient(colors: [Color(hex: "C07390"), Color(hex: "A74AA3"), Color(hex: "7332A9"), Color(hex: "4D2599"), Color(hex: "36208A")]), startPoint: .leading, endPoint: .trailing)
             )
-            HStack {
+
+            // Dashboard
+            HStack(spacing: 12) {
                 ForEach(daysOfWeek, id: \.self) { day in
-                    Text(day)
-                        .font(.system(size: day == dayToHighlight ? 20 : 15))
-                        .fontWeight(day == dayToHighlight ? .bold : .regular)
-                        .padding(.horizontal, 2)
+                    VStack {
+                        Rectangle()
+                            .fill(determineColor(for: day))
+                            .frame(width: 40, height: day == dayToHighlight ? 70 : 60)
+                            .cornerRadius(2)
+                                    
+                        Text(day)
+                            .font(.system(size: day == dayToHighlight ? 20 : 15))
+                            .fontWeight(day == dayToHighlight ? .bold : .regular)
+                    }
                 }
             }
-                        .padding(.vertical, 10)
+            .padding(.vertical, 30)
 
-
+            // Sección de rendimiento mensual
             Text("Monthly Performance")
-                .padding(.horizontal) // Aplica padding horizontal a este Text
+                .foregroundColor(.purple)
+            
+            let columns = Array(repeating: GridItem(.flexible()), count: 7)
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(0..<daysInMonth, id: \.self) { day in
+                    Button(action: {
+                        let today = Calendar.current.startOfDay(for: Date())
+                        if let dayToAdd = Calendar.current.date(byAdding: .day, value: day, to: today) {
+                            if selectedDays.contains(dayToAdd) {
+                                selectedDays.remove(dayToAdd)
+                            } else {
+                                selectedDays.insert(dayToAdd)
+                            }
+                        }
+                    }) {
+                        Text("\(day + 1)")
+                            .frame(width: 35, height: 35)
+                            .background(selectedDays.contains {
+                                Calendar.current.isDate($0, inSameDayAs: Calendar.current.date(byAdding: .day, value: day, to: Date())!)
+                            } ? Color.purple : Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(25)
+                    }
+                }
+            }
+            Spacer()
+            .padding(.horizontal)
+        }
+        .edgesIgnoringSafeArea(.top) // Ignora el área segura en la parte superior
+    }
 
-            // MultiDatePicker con su propio frame
-            MultiDatePicker("Label", selection: .constant([]))
-                .frame(width: 300, height: 600)
-                .padding(.horizontal)
+    func determineColor(for day: String) -> Color {
+        if day == dayToHighlight {
+            return Color(hex: "A74AA3")
+        } else if dayCompleted.contains(day) {
+            return Color(hex: "36208A")
+        } else {
+            return .gray // Un color por defecto si no se cumple ninguna condición
         }
     }
 }
+
 
 extension Color {
     init(hex: String) {
@@ -84,10 +139,8 @@ extension Color {
     }
 }
 
-// Preview
 struct HomePageView_Previews: PreviewProvider {
     static var previews: some View {
         HomePageView()
     }
 }
-

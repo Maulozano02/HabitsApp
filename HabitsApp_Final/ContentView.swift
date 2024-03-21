beed746b4fd9401494df8abb0267d2a7492104f3
EugeRuiz <eugeniarvo@gmail.com>
2024-03-21 12:03:13 -0600

    Log in page Update

--- HabitsApp_Final/ContentView.swift
+++ HabitsApp_Final/ContentView.swift
@@ -9,6 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var showPassword = false // New state to track password visibility
+    
     var body: some View {
         //Here comes the  connection to pruebaVM = pruebaViewModel
         
@@ -54,11 +58,28 @@ struct ContentView: View {
                         }
                         .padding(.top, 10.0)
                         
-                        TextField("Please enter your Password", text: .constant(""))
-                            .font(.system(size: 14)) // Adjust the size as per your requirement
-                            .padding(4) // Adds some top padding around the TextField for better appearance
-                            .border(Color.gray) // Optionally, add a border to make the TextField visible
                         
+                        ZStack(alignment: .trailing) {
+                            if showPassword {
+                                TextField("Please enter your Password", text: $password)
+                                    .font(.system(size: 14))
+                                    .padding(4)
+                                    .border(Color.gray)
+                            } else {
+                                SecureField("Please enter your Password", text: $password)
+                                    .font(.system(size: 14))
+                                    .padding(4)
+                                    .border(Color.gray)
+                            }
+                            
+                            Button(action: {
+                                showPassword.toggle()
+                            }) {
+                                Image(systemName: showPassword ? "eye.slash.fill" : "eye.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
                         
                         
                         HStack{
@@ -67,7 +88,7 @@ struct ContentView: View {
                             }
                             .toggleStyle(checkbox())
                             
-                            .padding(.trailing, 80.0)
+                            .padding(.trailing, 90.0)
                             .padding(.top, 10)
                             
                             
@@ -77,12 +98,65 @@ struct ContentView: View {
                             .font(.callout)
                             .padding(.top, 10)
                         }
+                        
                         NavigationLink(destination:HomePageView()){
                             Text("Login")
-                                .padding(15)
-                                .background(Color.purple)
+                                .padding(.horizontal, 150) // Adjust the horizontal padding to increase the width
+                                .padding(.vertical, 15)    // Adjust the vertical padding to increase the height
+                                .background(
+                                    LinearGradient(gradient: Gradient(colors: [Color(hex: "C07390"), Color(hex: "A74AA3"), Color(hex: "7332A9"), Color(hex: "4D2599"), Color(hex: "36208A")]), startPoint: .leading, endPoint: .trailing)
+                                )
                                 .foregroundColor(.white)
                                 .cornerRadius(20)
+                        }
+                        .padding(.top,140)
+                        .padding(.bottom, 20)
+                        
+                        HStack{
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(height: 0.8)
+                            Text(" Or With  ").font(.footnote).fontWeight(.light)
+                                .foregroundColor(.gray) // Set text color to gray
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(height: 0.8)
+                        }
+                        
+                        
+                        HStack {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 5) // Adjust the corner radius as needed
+                                    .stroke(Color.gray, lineWidth: 1) // Adjust the color and width of the border
+                                    .frame(width: 173, height: 35) // Adjust the width and height of the rectangle
+                                
+                                HStack {
+                                    Image("GoogleLogo")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 20, height: 20) // Adjust the size of the image as needed
+                                    
+                                    Text("Sign in with Google")
+                                        .font(.system(size: 14))
+                                        .padding(6)
+                                }
+                            }
+                            
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 5) // Adjust the corner radius as needed
+                                    .stroke(Color.gray, lineWidth: 1) // Adjust the color and width of the border
+                                    .frame(width: 173, height: 35) // Adjust the width and height of the rectangle
+                                
+                                HStack {
+                                    Image(systemName: "applelogo") // Using SF Symbol for Apple logo
+                                        .font(.system(size: 23))
+                                        .padding(.leading, 6) // Adjust padding as needed
+                                    
+                                    Text("Sign in with Apple")
+                                        .font(.system(size: 14))
+                                        .padding(6)
+                                }
+                            }
                         }
                         
                     })
@@ -111,6 +185,7 @@ struct checkbox: ToggleStyle {
         }
     }
 }
+
 
 #Preview {
     ContentView()
